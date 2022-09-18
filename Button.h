49a40af0ce2f27dc8febc93bcc90b75d3544a0dc
pinchy <sebeckmas@gmail.com>
2022-09-18 14:55:38 +1000

    cleaning header

--- Button.h
+++ Button.h
@@ -7,33 +7,34 @@
 
 class Button
 {
-    typedef void (*CallBack_t)();
+        typedef void (*CallBack_t)();
 
-private:
-    int _pin;
-    bool _state;
-    bool _previousState;
-    bool _inversed;
+    private:
+        int _pin;
+        bool _state;
+        bool _previousState;
+        bool _inversed;
 
-    uint32_t _lastEdge;
-    uint32_t _debounceTimeout = (uint32_t)BUTTON_DEFAULT_DEBOUNCE;
-    CallBack_t _CallBackOnPress = nullptr;
-    CallBack_t _CallBackOnRelease = nullptr;
+        unsigned long _lastEdge;
+        unsigned long _debounceTimeout = (unsigned long) BUTTON_DEFAULT_DEBOUNCE;
+        CallBack_t _CallBackOnPress = nullptr;
+        CallBack_t _CallBackOnRelease = nullptr;
 
-    void _handleCallBacks(void);
+        void _handleCallBacks(void);
 
-public:
-    Button();
-    Button(int p, bool i = false);
-    ~Button();
-    void begin(void);
+    public:
+        Button();
+        Button(int p, bool i = false);
+        ~Button();
+        void begin(void);
 
-    void setCallBack(CallBack_t cbp, CallBack_t cbr = nullptr);
+        void setCallBack(CallBack_t cbp, CallBack_t cbr = nullptr);
+        void setDebounceTimeout(unsigned long t) { this->_debounceTimeout = t; };
 
-    void setPin(int p, bool i = false);
-    bool pressed(void);
-    bool read(void);
-    void tick(void);
+        void setPin(int p, bool i = false);
+        bool pressed(void);
+        bool read(void);
+        void tick(void);
 };
 
 #endif
